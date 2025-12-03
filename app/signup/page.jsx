1b2914f4a66d2a8dfcbd8bfebc1fe64016838bb6
Shivansh <shivansh2162001@gmail.com>
2025-12-03 12:23:51 -0700

    wip:add  input fields for signup page

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -29,7 +29,69 @@ const SignupPage = () =>{
                 <h1 className='text-3xl text-orange-700 font-bold text-center mb-2'>Create Account</h1>
                 <p className='text-l text-gray-700 font-medium text-center'>Sign up to get started!</p>
                 {error && (<p className='p-3 bg-red-50 border border-red-200 text-center text-red-700 rounded-lg text-sm mb-4'>{error}</p>)}
-                 <form onSubmit={handleSubmit} className='space-y-4'></form>
+                 <form onSubmit={handleSubmit} className='space-y-4'>
+                    {/* Full Name field */}
+                    <div>
+                        <label htmlFor='name' className='text-sm font-bold text-gray-700 mb-1'>Full Name:</label>
+                        <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        required
+                        placeholder="Drax Johnson"
+                        className='w-full px-3 py-2 border border-gray-500 rounded-md shadow-sm text-base text-gray-700 placeholder-gray-400
+                        focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500'
+                        value={formData.name}
+                        onChange={handleChange}
+                        />
+
+                    </div>
+                    {/* Email Field */}
+                    <div>
+                        <label htmlFor='email' className='text-sm font-bold text-gray-700 mb-1'>Email:</label>
+                        <input 
+                            type="email" 
+                            id="email"
+                            name="email" 
+                            required 
+                            value={formData.email} 
+                            onChange={handleChange} 
+                            placeholder='you@example.com'
+                            className='w-full px-3 py-2 border border-gray-500 rounded-md shadow-sm text-base text-gray-700 placeholder-gray-400
+                            focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500'
+                            />
+                    </div>
+                    {/* password field */}
+                    <div>
+                        <label htmlFor='password' className='text-sm font-bold text-gray-700 mb-1'>Password:</label>
+                        <input 
+                            type="password" 
+                            id="password" 
+                            name="password"
+                            required 
+                            value={formData.password} 
+                            onChange={handleChange}
+                            placeholder='••••••••'
+                            className='w-full px-3 py-2 border border-gray-500 rounded-md shadow-sm text-base text-gray-700 placeholder-gray-400
+                            focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500'
+                            />
+                    </div>
+                     {/* confirm password field */}
+                    <div>
+                        <label htmlFor='confirmPassword' className='text-sm font-bold text-gray-700 mb-1'>Confirm Password:</label>
+                        <input 
+                            type="password" 
+                            id="confirmPassword" 
+                            name="confirmPassword"
+                            required 
+                            value={formData.confirmPassword} 
+                            onChange={handleChange}
+                            placeholder='••••••••'
+                            className='w-full px-3 py-2 border border-gray-500 rounded-md shadow-sm text-base text-gray-700 placeholder-gray-400
+                            focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500'
+                            />
+                    </div>
+                 </form>
             </div>
         </div>
     )
